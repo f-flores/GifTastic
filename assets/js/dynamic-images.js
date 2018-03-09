5d384a519384980a17235cc5fcf90d0dc32b92ea
Fabian Flores <ffflores1@outlook.com>
2018-03-08 23:35:22 -0500

    Initial version of displaying giphy images and pausing/unpausing images.

--- assets/js/dynamic-images.js
+++ assets/js/dynamic-images.js
@@ -14,6 +14,113 @@
  //       AnswerShow = 8,
  //       BeginWait = 1.5;
   console.log("in document ready");
-  //  $(document).on("click", ".trivia-choice-button", getTriviaChoice);
+
+  // --------------------------------------------------------------------------------------
+  // $(".gif").on("click"..) enables the pausing-gifs effect. It toggles between a "still"
+  // and "animated" data-state.
+  //
+  // $(".gif-image").on("click", () => {
+  function pauseImgEffect() {
+    var animatedURL = $(this).attr("data-animate"),
+        stillURL = $(this).attr("data-still"),
+        state = $(this).attr("img-state");
+
+    console.log("Image state: " + state);
+    console.log("Animated URL: " + animatedURL);
+    console.log("Still URL: " + stillURL);
+
+    // If data-state equals still, then the animated gif is activated and the "data-state"
+    // attribute is switched to "animate". Otherwise, if the "data-state" is set to "animate",
+    // the still image reappears, and the "data-state" is switched back to "still."
+    if (state === "still") {
+      $(this).attr("src", animatedURL);
+      $(this).attr("img-state", "animate");
+    } else {
+      $(this).attr("src", stillURL);
+      $(this).attr("img-state", "still");
+    }
+  }
+//  });
+
+  // --------------------------------------------------------------------------------------
+  // renderImgs appends the topic related imgs to the dom
+  //
+  function renderImgs(data) {
+    var img,
+        index;
+
+    console.log("in renderImgs() -- data: " + data);
+    for (index = 0; index < data.length; index++) {
+      // create img tag
+      img = $("<img>");
+      console.log("Title: " + data[index].title);
+
+      // add src and alt attributes to image, and custom attribute img-state
+      img.attr("src", data[index].images.original_still.url);
+      img.attr("alt", data[index].title);
+      img.attr("img-state", "still");
+
+      // add data-still and data-animate attributes to store respective image urls
+      img.attr("data-still", data[index].images.original_still.url);
+      img.attr("data-animate", data[index].images.original.url);
+
+      // add gif class, which will later enable toggling still and animated state
+      // img-fluid leverages bootstrap 4's image responsiveness
+      img.addClass("gif-image");
+
+      // append to DOM's "#topic-images"
+      $("#topic-images").append(img);
+    }
+  }
+
+  // Function for dumping the JSON content for each button into the div
+  function displayTopicImgs() {
+    // YOUR CODE GOES HERE!!! HINT: You will need to create a new div to hold the JSON.
+    // $("#current-movie").remove(); // remove div current-movie
+
+    // var movieDiv = $("<div>");
+    // movieDiv.attr("id","current-movie");
+    // GIPHYLIMIT = 10,
+    //  GIPHYKEY = "zOxVha9Ha82FHhEMPSbIBvoOOApcLrBK",
+    //  GIPHYURL = "https://api.giphy.com/v1/gifs/search?" + GIPHYKEY;
+    //  GIPHYSUFFIX = "&limit=" + GIPHYLIMIT.toString() + "&offset=0&rating=G&lang=en";
+    var topicName = $(this).attr("data-name"),
+        queryURL = GIPHYURL + topicName + GIPHYSUFFIX,
+        result;
+
+        console.log("in displayTopicImgs() queryURL: " + queryURL);
+      $.ajax({
+        "method": "GET",
+        "url": queryURL
+      }).then((response) => {
+        console.log(response);
+        result = response.data;
+        renderImgs(result);
+      });
+
+  }
+
+
+  // --------------------------------------------------------------------------------------
+  // handles when a topic is entered in the input text field and submitted
+  //
+  $("#add-topic-btn").on("click", (event) => {
+    var currentTopic;
+
+    event.preventDefault();
+
+    // grabs the input from the topic textbox
+    currentTopic = $("#topic-input").val().trim();
+
+    // The topic from the textbox input is added to array
+    topicsArray.push(currentTopic);
+
+    // call to renderButtons handles topicsArray
+    renderButtons();
+
+  });
+
+  $(document).on("click", ".topic", displayTopicImgs);
+  $(document).on("click", ".gif-image", pauseImgEffect);
 
 });
--- assets/js/globals.js
+++ assets/js/globals.js
@@ -5,14 +5,28 @@
 //
 // *************************************************************************************
 
-// grabs the localStorage object in 'array' parsed format
-var topicsList = JSON.parse(localStorage.getItem("giftasticTopics"));
+// **************************
+// Global constants
+// **************************
+const GIPHYLIMIT = 10,
+      GIPHYKEY = "zOxVha9Ha82FHhEMPSbIBvoOOApcLrBK",
+      GIPHYURL = "https://api.giphy.com/v1/gifs/search?api_key=" + GIPHYKEY + "&q=",
+      GIPHYSUFFIX = "&limit=" + GIPHYLIMIT.toString() + "&offset=0&rating=G&lang=en";
+
+// grabs the localStorage object in the *global* topicsList 'array' in parsed format
+var topicsList = JSON.parse(localStorage.getItem("giftasticTopics")),
+    favList = JSON.parse(localStorage.getItem("gifTasticFavorites")),
+    topicsArray = ["cat","dog","dolphin","hamster","hen","parakeet"];
 
 // -------------------------------------------------------------------------------------
-// checkLocalStorage() checks if gifTastic exists in localStorage and if it is an array;
-// if it does not already exist then it is initialized to an empty array
+// checkLocalStorage() checks if gifTasticTopics and gifTasticFavorites exist
+// in localStorage and if they are an array;
+// if they do not already exist then array is initialized to an empty array
 function checkLocalStorage() {
   if (!Array.isArray(topicsList)) {
+    topicsList = [];
+  }
+  if (!Array.isArray(favList)) {
     topicsList = [];
   }
 }
@@ -23,5 +37,33 @@ function insertImages() {
   console.log("in InsertImages()");
 }
 
+
+// Function for displaying topic data
+function renderButtons() {
+  var index,
+      btn;
+
+  // Deleting the buttons prior to adding new topics
+  // so we don't have repeat buttons
+  $("#topic-list").empty();
+
+  // Looping through the array of movies
+  for (index = 0; index < topicsArray.length; index++) {
+
+    // Dynamically create button for each topic
+    btn = $("<button>");
+
+    // Adding a class of topic to button
+    btn.addClass("topic");
+    // Adding a data-attribute
+    btn.attr("data-name", topicsArray[index]);
+    // Providing the initial button text
+    btn.text(topicsArray[index]);
+    // Adding the button to the buttons-view div
+    $("#topic-list").append(btn);
+  }
+}
+
 checkLocalStorage();
 insertImages();
+renderButtons();
